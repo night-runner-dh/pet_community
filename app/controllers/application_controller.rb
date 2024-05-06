e83e7a2f465199d24c12037f3bfc5cf12492b7f2
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-05-06 03:52:21 +0000

    ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+ before_action :authenticate_admin!, if: :admin_url
+
+def admin_url
+  request.fullpath.include?("/admin")
+end
+
+
+def after_sign_in_path_for(resource)
+  case resource
+   when Admin
+     admin_path
+   when User
+     public_homes_top_path
+   else
+     root_path
+  end
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 会員用
+# URL /users/sign_in ...
+devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+
+root to: 'public/homes#top'
+
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :admin do
+    get 'homes/top'
+    get 'homes/about'
+  end
+
+
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
