4ef028ce1e56e784e5571ea79b86cfdb3f61b841
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-05-07 04:33:05 +0000

    ビュー修正中1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  #バリデーション、新規登録時は名前とアドレスのみなので二つのみ
+  validates :name, presence: true
+  #validates :email, presence: true
+  
 end
