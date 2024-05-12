7c15cc6a5566f60e6980e2616506c5fbb697f1e9
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-05-12 01:50:28 +0000

    実装フェーズ1完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.find_or_create_by!(email: ENV['admintest@yahoo.co.jp']) do |admin|
+  admin.password = ENV['123456']
+end
+
+User.find_or_create_by!(email: 'test@yahoo.co.jp') do |user|
+  user.password = '123456'
+  user.name = 'test'
+end
