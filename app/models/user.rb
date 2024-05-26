a3fd2090dfa7fe8a74710791deb2278c08bff327
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-05-26 21:04:37 +0000

    グループ機能試作中10

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,10 +59,10 @@ class User < ApplicationRecord
   end
   
   def get_profile_image
-  unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/pet_sample2.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'pet_sample2.jpg', content_type: 'image/jpeg')
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [200, 200]).processed
   end
-  profile_image.variant(resize_to_limit: [200, 200]).processed
-end
 end
