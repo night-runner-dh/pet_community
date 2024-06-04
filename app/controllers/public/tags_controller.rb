93cfc966096ea39843942b90d0fa6b35955e3cf6
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-06-04 02:53:21 +0000

    タグ検索時降順へ修正

--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -1,7 +1,7 @@
 class Public::TagsController < ApplicationController
   def index
   @tag = Tag.find_by(name: params[:name])
-  @posts = Post.where('id IN (SELECT post_id FROM tags WHERE name = ?)', params[:name]).page(params[:page]).per(5)
+  @posts = Post.where('id IN (SELECT post_id FROM tags WHERE name = ?)', params[:name]).order(created_at: :desc).page(params[:page]).per(5)
   @tags = Tag.unique_tags
   end
 end
